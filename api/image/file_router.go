f8e5a2815bc9d1ec960e6a67f32c3027ff8f6098
liang <liangliang@bmos.net.cn>
2025-01-23 13:41:40 +0800

    添加图片上传功能

--- api/image/file_router.go
+++ api/image/file_router.go
@@ -1,6 +1,7 @@
 package image
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"la-blog-go/api"
 	"la-blog-go/response"
@@ -12,6 +13,8 @@ var (
 			Method: "POST",
 			Url:    "/upload",
 			Func: func(c *gin.Context) {
+				file, _ := c.FormFile("file")
+				fmt.Println(file.Filename)
 				response.SuccessWithData(c, "上传成功", "http://172.30.1.160/front-end/assets/logo/bmos/Bmos_logo.svg")
 			},
 		},
